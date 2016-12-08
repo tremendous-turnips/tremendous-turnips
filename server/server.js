1f902e598408d88c51cae7c2b4ef2d5bedd473c2
David Wayman <mrdavidwayman@gmail.com>
2016-12-07 21:18:14 -0800

    Fixes merge conflicts

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 var path = require('path');
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 var port = process.env.PORT || 1337;
 
@@ -15,13 +17,27 @@ app.get('/', function(req, res) {
 app.get('/lobby', function(req, res) {
   // get chatroom data from database and serve to client
   res.send(JSON.stringify({data: 'placeholder for db fetch'}));
-  // NEED TO REQUIRE DB CONNECTION >>>>>>>>>>>>>>>>>>>>>>>>   
+  // NEED TO REQUIRE DB CONNECTION >>>>>>>>>>>>>>>>>>>>>>>>
   // db.Chatroom.findAll()
   //   .then(function(rooms) {
   //     res.status(200).send(rooms);
   //   });
 });
 
-app.listen(port, function() {
+////////////////////////////////////////////////////////////////////////////////
+// SOCKET.IO
+////////////////////////////////////////////////////////////////////////////////
+io.on('connection', function (socket) {
+  socket.emit('connected', 'you connected to the server!');
+  socket.on('typingMessage', function (data) {
+    console.log('this is what is getting typed: ', data);
+    socket.emit('listeningForMessage', data);
+  })
+});
+
+app.use(express.static('socket.io'));
+////////////////////////////////////////////////////////////////////////////////
+
+server.listen(port, function() {
   console.log('Server running on port', port);
 });
