6c97d17d544f5690668c5aefc5bf39c9d559b774
jamesgu <jamesgu521@gmail.com>
2016-12-07 21:11:27 -0800

    Implement ajax request (to be modified for data format)
    Also add jquery src to index.html

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,5 +1,5 @@
 var services = angular.module('argue.services', []);
-
+var baseUrl = 'http://127.0.0.1:1337';
 
 services.factory('Logins', function() {
   return {};
@@ -9,8 +9,28 @@ services.factory('Lobby', function($location) {
   var redirect = function(path) {
     $location.path(path);
   }
+  
+  var fetchRooms = function(callback) {
+    
+    // PLACEHOLDER AJAX CALL, EXPECTS "{data: 'somedata'}"
+    $.ajax({
+      url: baseUrl + '/lobby',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function(rooms) {
+        console.log('successful get');
+        callback(JSON.parse(rooms).data);
+      },
+      error: function(err) {
+        console.log(baseUrl + '/lobby')
+        console.log('Fetch to server got cucked');
+      }
+    });
+  }
+
   return {
-    redirect: redirect
+    redirect: redirect,
+    fetchRooms: fetchRooms
   };
 });
 
