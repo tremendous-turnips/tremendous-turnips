89ae20fbb09986e4c0f88e23cd7ab9786bf1fef0
David Wayman <mrdavidwayman@gmail.com>
2016-12-08 14:49:22 -0800

    Implements web socket realtime chat

--- client/app/chatroom/chatroom.js
+++ client/app/chatroom/chatroom.js
@@ -1,19 +1,29 @@
 var chatroom = angular.module('argue.chatroom', []);
 
+///////////////////////////////////////////////////////////
+// Socket.io event listeners
+///////////////////////////////////////////////////////////
+var socket = io('/chatroom');
+// Listens for a new message from the server
+socket.on('posted message', function(msg){
+  $('.messageList').append($('<li>').text(msg));
+});
+///////////////////////////////////////////////////////////
+// Chatroom Controller
+///////////////////////////////////////////////////////////
 chatroom.controller('chatroomController', function($scope, $location, Chatroom) {
   Chatroom.validateUser(function(result) {
     $scope.myuser = result.data;
   }).then(function() {
     if ($scope.myuser !== '') {
-      var socket = io();
 
-      socket.on('connected', function (data) {
-        console.log(data);
-      });
+      $scope.postMessage = function() {
+        // submit a post request to the server to send the message
+        socket.emit('chat message', $scope.userMessage); // This is a socket, not post request
 
-      socket.on('listeningForMessage', function (data) {
-        $scope.opponentCurrentMessage = data;
-      });
+        // Clear message text box
+        $('.messageTextBox').val('');
+      };
 
       $scope.sendMessage = function() {
         socket.emit('typingMessage', $scope.userMessage);
