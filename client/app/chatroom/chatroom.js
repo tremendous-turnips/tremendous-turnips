a12557049f46191bf253ed1042d553765ed45245
David Wayman <mrdavidwayman@gmail.com>
2016-12-10 13:35:20 -0800

    Improves chatroom UI with scroll-glue directive

--- client/app/chatroom/chatroom.js
+++ client/app/chatroom/chatroom.js
@@ -1,4 +1,6 @@
-var chatroom = angular.module('argue.chatroom', []);
+var chatroom = angular.module('argue.chatroom', [
+  'luegg.directives' // scroll-glue directive: https://github.com/Luegg/angularjs-scroll-glue
+]);
 
 ///////////////////////////////////////////////////////////
 // Socket.io event listeners
@@ -20,9 +22,13 @@ chatroom.controller('chatroomController', function($scope, $location, $http, Cha
     if ($scope.myuser !== '') {
       $scope.roomName = Chatroom.currRoom;
       console.log('this is roomname', $scope.roomName);
+
+      // Set focus to input text field so user doesn't need to click on it to type
+      $('.messageTextBox').focus();
+
       $scope.leaveRoom = function() {
         $location.path('/token');
-      }
+      };
 
       $scope.postMessage = function() {
         // Emit a socket event to send the message with the username and text
@@ -39,7 +45,7 @@ chatroom.controller('chatroomController', function($scope, $location, $http, Cha
 
       $scope.sendMessage = function() {
         socket.emit('typingMessage', $scope.userMessage);
-      }
+      };
     } else {
       $location.path('/login');
     }
