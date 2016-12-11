d4cabd0479e5510345a1b6859fcdfe36c815ec4a
Matt Walsh <walsh.matt.t@gmail.com>
2016-12-10 16:54:49 -0800

    Remove status from chatroom schema. Add dummy messages.

--- server/app/config.js
+++ server/app/config.js
@@ -4,11 +4,6 @@ var sequelize = new Sequelize('turnip', 'root', process.env.MYSQL_PASSWORD, {
   dialect: 'mysql'
 });
 
-// var User = sequelize.define('users', {
-//   username: {type: Sequelize.STRING, field: 'username'},
-//   image_url: {type: Sequelize.STRING,field: 'image_url'}
-// });
-
 var Chatroom = sequelize.define('chatrooms', {
   roomName: {type: Sequelize.STRING, field: 'room_name'},
   firstUser: {type: Sequelize.STRING, field: 'first_user'},
@@ -22,13 +17,10 @@ var Message = sequelize.define('messages', {
   chatroom: {type: Sequelize.STRING}
 });
 
-// Message.belongsTo(User);
-// Message.belongsTo(Chatroom);
-// User.belongsTo(Chatroom);
-// Chatroom.belongsTo(User, {as: 'firstUser'});
-// Chatroom.belongsToMany(Message);
-// User.hasMany(Message, {as: 'messages'});
-// User.hasMany(Chatroom, {as: 'chatrooms'});
+// var User = sequelize.define('users', {
+//   username: {type: Sequelize.STRING, field: 'username'},
+//   image_url: {type: Sequelize.STRING,field: 'image_url'}
+// });
 
 // User.sync({force: false}).then(function () {
 //   // User.create({
@@ -47,44 +39,64 @@ var Message = sequelize.define('messages', {
 Chatroom.sync({force: true}).then(function () {
   Chatroom.create({
     roomName: 'Hillary v Donald',
-    status: 'closed',
     firstUser: '_____',
     secondUser: '_____'
   });
   Chatroom.create({
     roomName: 'Emacs v Vim',
-    status: 'closed',
     firstUser: '_____',
     secondUser: '_____'
   });
   Chatroom.create({
     roomName: 'McDonalds v Burger King',
-    status: 'closed',
     firstUser: '_____',
     secondUser: '_____'
   });
   Chatroom.create({
     roomName: 'Coke v Pepsi',
-    status: 'closed',
     firstUser: '_____',
     secondUser: '_____'
   });
   Chatroom.create({
     roomName: 'Blue v Black',
-    status: 'closed',
     firstUser: '_____',
     secondUser: '_____'
   });
 });
 
-Message.sync({force: false}).then(function () {
-  // Message.create({
-  //   text: 'They both suck.',
-  //   userId: 1,
-  //   chatroomId: 1
-  // });
+Message.sync({force: true}).then(function () {
+  Message.create({
+    text: 'Hi!',
+    user: 'James',
+    chatroom: 'Hillary v Donald'
+  });
+  Message.create({
+    text: 'Hey!',
+    user: 'Matt',
+    chatroom: 'Hillary v Donald'
+  });
+  Message.create({
+    text: 'How are you?',
+    user: 'James',
+    chatroom: 'Hillary v Donald'
+  });
+  Message.create({
+    text: 'Good. How about you?',
+    user: 'James',
+    chatroom: 'Hillary v Donald'
+  });
+  Message.create({
+    text: 'Do you like Trump?',
+    user: 'James',
+    chatroom: 'Hillary v Donald'
+  });
+  Message.create({
+    text: 'Not really.',
+    user: 'James',
+    chatroom: 'Hillary v Donald'
+  });
 });
 
 module.exports.Chatroom = Chatroom;
-// module.exports.User = User;
 module.exports.Message = Message;
+// module.exports.User = User;
