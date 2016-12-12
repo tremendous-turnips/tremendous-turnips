17402116efafb4871dafea749bdeee92ce79457b
jamesgu <jamesgu521@gmail.com>
2016-12-12 11:02:09 -0800

    Modularize updating users in lobby rooms view

--- server/server.js
+++ server/server.js
@@ -25,31 +25,7 @@ app.get('/lobby', function(req, res) {
 });
 
 app.put('/lobby', function(req, res) {
-  db.Chatroom.find({ where: { roomName: req.body.chatroomName } })
-  .then(function(room) {
-    if (room) {
-      // Set session variables to be able to delete name after leaving / logging out
-      req.session.user = req.body.user;
-      req.session.chatroomName = req.body.chatroomName;
-
-      // Update chatroom depending on if entering as user 1 or user 2
-      if (req.body.user === 1) {
-        room.updateAttributes({
-          firstUser: req.body.username,
-        });
-      } else {
-        room.updateAttributes({
-          secondUser: req.body.username
-        });
-      }
-    } else {
-      res.send('Error on updating given chatroom name');
-    }
-  })
-  .then(function(room) {
-    console.log('Successfully posted new username into db chatrooms');
-    res.send(room);
-  });
+  ChatroomCtrl.updateLobbyRooms(req, res);
 });
 
 // ROUTE TO DO >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
